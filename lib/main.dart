8e0fa2d5e45b8442e3f4a83abd0de5d40205aae3
RhysRoo <rhys.bundy97@gmail.com>
2023-12-01 14:43:17 +0000

    removing_old_code

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'node.dart';
+// import 'package:flutter/services.dart';
+// import 'node.dart';
 import 'home_page.dart';
 
-List<Node> decisionMap = [];
+// List<Node> decisionMap = [];
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //POPULATE LIST CODE
-  String csv = "assets/decision_map.csv";
-  String fileData = await rootBundle.loadString(csv);
-  print(fileData); //test data is loaded.
+  // String csv = "assets/decision_map.csv";
+  // String fileData = await rootBundle.loadString(csv);
+  // print(fileData); //test data is loaded.
 
-  List<String> rows = fileData.split("\n");
-  for (int i = 0; i < rows.length; i++) {
-    //selects an item from row and places
-    String row = rows[i];
-    List<String> itemInRow = row.split(",");
+  // List<String> rows = fileData.split("\n");
+  // for (int i = 0; i < rows.length; i++) {
+  //   //selects an item from row and places
+  //   String row = rows[i];
+  //   List<String> itemInRow = row.split(",");
 
-    Node node =
-        Node(int.parse(itemInRow[0]), int.parse(itemInRow[1]), itemInRow[2]);
+  //   Node node =
+  //       Node(int.parse(itemInRow[0]), int.parse(itemInRow[1]), itemInRow[2]);
 
-    decisionMap.add(node);
-  }
+  //   // decisionMap.add(node);
+  // }
 
   runApp(
     const MaterialApp(
--- lib/user_post.dart
+++ lib/user_post.dart
@@ -6,6 +6,7 @@ class UserPost extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('Post Information'),
       ),
