4922771cf2e272ff250388ee9d2eba3ad7f0df4d
RhysRoo <93945223+RhysRoo@users.noreply.github.com>
2024-01-14 00:10:22 +0000

    working_on_signup_upload_image_btn

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -30,7 +30,38 @@ class Signup extends StatelessWidget {
                 ),
               ),
             ),
-            Row(children: []),
+            Align(
+              alignment: Alignment(0.0, -0.7),
+              child: Text(
+                '     Before we start, let\'s get you seen. \n below could you upload a profile picture\n                              for us :)',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment(0.0, -0.53),
+              child: Icon(
+                Icons.camera_alt_outlined,
+                size: 80,
+              ),
+            ),
+            Column(children: [
+              SizedBox(height: 300),
+              Align(
+                alignment: Alignment(-0.8, 0.0),
+                child: Text(
+                  'Email',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ]),
           ],
         ),
       ),
